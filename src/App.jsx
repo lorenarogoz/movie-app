fd8bc367a7550bfea83a63006ff506078908af4d
Rogoz Lorena <Lorena.Rogoz@cognizant.com>
2026-02-11 16:54:57 +0200

    The implementation for watchlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,66 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MovieGrid from './components/MovieGrid';
+import Watchlist from './components/Watchlist';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 
 function App() {
+    const [movies, setMovies] = useState([]);
+    const [watchlist, setWatchlist] = useState([]);
+
+    useEffect(() => {
+        fetch('movies.json')
+            .then((response) => response.json())
+            .then((data) => setMovies(data));
+    }, []);
+
+    const toggleWatchlist = (movieId) => {
+        setWatchlist((prev) =>
+            prev.includes(movieId)
+                ? prev.filter((id) => id !== movieId)
+                : [...prev, movieId],
+        );
+    };
+
     return (
         <div className='App'>
             <div className='container'>
                 <Header></Header>
-                <MovieGrid></MovieGrid>
+                <Router>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/watchlist'>WatchList</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route
+                            path='/'
+                            element={
+                                <MovieGrid
+                                    movies={movies}
+                                    watchlist={watchlist}
+                                    toggleWatchlist={toggleWatchlist}
+                                />
+                            }
+                        ></Route>
+                        <Route
+                            path='/watchlist'
+                            element={
+                                <Watchlist
+                                    movies={movies}
+                                    watchlist={watchlist}
+                                    toggleWatchlist={toggleWatchlist}
+                                />
+                            }
+                        ></Route>
+                    </Routes>
+                </Router>
             </div>
             <Footer></Footer>
         </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function MovieCard({movie}) {
+export default function MovieCard({movie, isWatchlisted, toggleWatchlist}) {
     const handlerError = (e) => {
         e.target.src = 'images/default.jpg';
     };
@@ -14,8 +14,24 @@ export default function MovieCard({movie}) {
             />
             <div className='movie-card-info'>
                 <h3 className='movie-card-title'>{movie.title}</h3>
-                <p className='movie-card-genre'>{movie.genre}</p>
-                <p className='movie-card-rating'>{movie.rating}</p>
+                <div>
+                    <span className='movie-card-genre'>{movie.genre}</span>
+                    <span className='movie-card-rating'>{movie.rating}</span>
+                </div>
+                <label className='switch'>
+                    <input
+                        type='checkbox'
+                        checked={isWatchlisted}
+                        onChange={() => toggleWatchlist(movie.id)}
+                    ></input>
+                    <span className='slider'>
+                        <span className='slider-label'>
+                            {isWatchlisted
+                                ? 'In watchlist'
+                                : 'Add to watchlist'}
+                        </span>
+                    </span>
+                </label>
             </div>
         </div>
     );
